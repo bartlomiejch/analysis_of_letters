7d4e9de1254f2695bc2fb4b4f6790a10148bcc19
bartek <bartek.chowaniec@gmail.com>
2016-09-24 23:15:28 +0200

    Add analize function to viwes.py

--- letters_project/letters_app/views.py
+++ letters_project/letters_app/views.py
@@ -1,7 +1,17 @@
+import string
+import operator
+from collections import Counter
 from django.shortcuts import render_to_response
 from letters_app.models import Letter
 from letters_app.forms import LetterForm
 from django.core.context_processors import csrf
+
+
+def analize(text):
+    my_list = text.split()
+    words_only_list = [word.strip(string.punctuation) for word in my_list]
+    amount = Counter(words_only_list)
+    return amount.most_common(3)
 
 
 def get_letter(request):
@@ -13,8 +23,20 @@ def get_letter(request):
             my_model.text = form.cleaned_data['text']
             my_model.author = form.cleaned_data['author']
             my_model.save()
-            letters = Letter.objects.all()
-            args = {'letters': letters}
+            words = analize(form.cleaned_data['text'])
+            first = words[0][0]
+            first_amount = words[0][1]
+            second = words[1][0]
+            second_amount = words[1][1]
+            third = words[2][0]
+            third_amount = words[2][1]
+            args = {}
+            args['first'] = first
+            args['first_amount'] = first_amount
+            args['second'] = second
+            args['second_amount'] = second_amount
+            args['third'] = third
+            args['third_amount'] = third_amount
             return render_to_response('result.html', args)
     else:
         form = LetterForm
